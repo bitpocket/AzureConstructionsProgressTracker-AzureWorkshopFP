25706b5f552acd1d5573be5242dd202fa5fbd5bc
bitpocket <te-training@outlook.com>
2015-11-17 10:55:06 +0100

    init storage

--- AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
+++ AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web;
 
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
+
 namespace AzureConstructionsProgressTracker.Features.ProgressTracking
 {
     public class FilesStorageService
@@ -18,7 +24,26 @@ namespace AzureConstructionsProgressTracker.Features.ProgressTracking
             // - create container
             // - upload blob
             // - return URL
-            throw new NotImplementedException();
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse( ConfigurationManager.AppSettings["StorageConnectionString"]);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("myblob");
+
+            // Create or overwrite the "myblob" blob with contents from a local file.
+
+             blockBlob.UploadFromStream(file.InputStream);
+  
+
+
+            //CloudStorageAccount storageAccount = CloudStorageAccount.Parse( 
+            //    CloudConfigurationManager.GetSetting("DefaultEndpointsProtocol=https;AccountName=fpstorage1;AccountKey=ZDEh3F4G5zI/M7QWn5yhXP6cGNqjFJ823OF1S7ML0+hZxy7KZhuAxX1vTA65v0Q4pDF/9UrNigKpp5POttBGrg=="));
         }
     }
 }
